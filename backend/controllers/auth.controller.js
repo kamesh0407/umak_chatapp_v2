fdf79ffbcf8dec38a800c1857192214fe1fcb888
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-06 18:00:55 +0530

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    console.log("SignupUser");
+}
+
+export const login = (req, res) => {
+    console.log("loginUser");
+}   
+
+export const logout = (req, res) => {
+    console.log("logoutUser");
+}
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/signup",signup);
+
+router.get("/login");
+
+router.get("/logout");
+
+export default router;
