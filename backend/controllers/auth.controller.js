d8f26e233a62a300af5b8c49e6c6869443a0af23
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-07 23:26:34 +0530

    logout endpoint created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,6 +88,12 @@ export const login = async (req, res) => {
    }
 }   
 
-export const logout = (req, res) => {
-    console.log("logoutUser");
+export const logout =  (req, res) => {
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller: ",error.message);
+       res.status(500).json({error:"Internal Server Error"});
+    }
 }
