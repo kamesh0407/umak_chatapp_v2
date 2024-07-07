9f51577ef367786b1b9e82d01738dfc4120b8630
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-07 18:18:10 +0530

    MongoDB setup

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("connected to MongoDB");   
+      } catch (error) {
+        console.log("Error connecting to MongoDB: ",error.message);
+        
+    }
+};
+
+export default connectToMongoDB;    
