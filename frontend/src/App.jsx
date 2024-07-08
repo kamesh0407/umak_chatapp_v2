6a746b766643296eb5c1f991ff294cc45198f4ac
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-08 17:05:19 +0530

    created loginPage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
+import Login from "./pages/login/Login.jsx";
 
 function App() {
   
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-     
+     <Login />
     </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto '> 
+     <div className=" w-full p-6 bg-gray-100 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
+
+    {/* </div> <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm backdrop-opacity-10'> */}
+        <h1 className='text-3xl font-semibold text-center  text-gray-300'>
+          Login <br />
+         
+          <span className="text-yellow-700">
+             Umak ChatApp
+          </span>
+        </h1>
+
+        <form >
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Usename</span>
+            </label>
+            <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+
+          </div>
+        </form>
+
+      </div>
+    </div>
   )
 }
 
