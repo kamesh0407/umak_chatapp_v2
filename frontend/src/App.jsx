9d9a7195da802e5a5e0d9365617e41b70969133c
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-08 22:10:45 +0530

    signup page UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-import Login from "./pages/login/Login.jsx";
+import SignUp from './pages/signup/SignUp'
 
 function App() {
   
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-     <Login />
+     <SignUp/>
     </div>
   )
 }
---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,21 @@
+const GenderCheckbox = () => {
+return (
+    <div className="flex">
+        <div className="form-control">
+            <label className="label gap-2 cursor-pointer">
+                <span className="label-text">Male</span>
+                <input type="checkbox" className="checkbox border-slate-900" />
+            </label>
+        </div>
+        <div className="form-control ml-2">
+            <label className="label gap-2 cursor-pointer">
+                <span className="label-text">Female</span>
+                <input type="checkbox" className="checkbox border-slate-900" />
+            </label>
+        </div>
+    </div>
+);
+}
+
+export default GenderCheckbox;
+
