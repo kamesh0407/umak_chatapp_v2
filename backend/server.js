dc29fa156dcf5703e50524066d1067b1c715dfb2
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-10 14:37:06 +0530

    completed run build

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //this is package imports, used to start the server
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 
 //this is our routes, used to handle different routes
 import authRoutes from "./routes/auth.routes.js";
@@ -12,7 +14,10 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+
 const PORT =  process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 
 dotenv.config();
@@ -24,10 +29,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello World");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
 
 
 
