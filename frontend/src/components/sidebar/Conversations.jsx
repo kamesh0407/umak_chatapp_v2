7d2245ffd26b7bef53b1798209c284a34868549a
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-10 16:28:17 +0530

    scrollbar modified

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,14 @@
 import useGetConversations from "../../hooks/useGetConversations";
 import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation"
-import './hideScrollbar.css';
-
+import "./customScrollbar.css";
+// import './hideScrollbar.css';
+// hide-scrollbar
 const Conversations = () => {
  const {loading, conversations} = useGetConversations();
 
   return (
-    <div className="py-2 flex flex-col overflow-auto hide-scrollbar">
+    <div className="py-2 flex flex-col overflow-auto custom-scrollbar">
 
         {conversations.map((conversation,idx) => ( 
           <Conversation
