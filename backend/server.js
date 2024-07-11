d01312d1df161fa9c4ca2e0c9922246dffe3bdf7
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-11 12:07:19 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,13 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 
-const PORT =  process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-
 
 dotenv.config();
+
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT =  process.env.PORT || 5000;
+
 
 app.use(express.json()); //middleware to parse json data (from req.body)
 app.use(cookieParser()); //middleware to parse cookies
